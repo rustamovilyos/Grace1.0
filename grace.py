ae32544111da10326092becfd5bfb0ca36a4f45c
rustamovilyos <rustamov7871@gmail.com>
2023-07-19 08:55:30 +0500

    vaqtni aniq aytib berish funksiyasi to'liq realizatsiya qilindi

--- grace.py
+++ grace.py
@@ -1,4 +1,7 @@
 # GRACE
+import os
+import subprocess
+
 import config
 import stt
 import tts
@@ -8,40 +11,53 @@ import datetime
 import webbrowser
 import random
 
-tts.oa_speak(f'{config.OA_NAME} sizning xizmatingizda. ..')
-# Grace from Peaky Blinders
+from functions.soat import num2words, eng2uzb
 
+tts.oa_speak(f'{config.OA_NAME} sizning xizmatingizda. ..')
+
+
+# Grace from Peaky Blinders
+# oa - ovozli assistent
 def oa_respond(voice: str):
-    print(voice)
+    print("You said: ", voice)
     if voice.startswith(config.OA_ALIAS):
         # обращаются к ассистенту
         cmd = recognize_cmd(filter_cmd(voice))
 
         if cmd['cmd'] not in config.OA_CMD_LIST.keys():
-            tts.oa_speak("Buyruq, mandagi buyruqlar qatorida yo'q. ..")
+            tts.oa_speak("Buyruq, mendagi buyruqlar ro'yxatida yo'q. ..")
         else:
             execute_cmd(cmd['cmd'])
 
 
 def filter_cmd(raw_voice: str):
+    # print("raw voice: ", raw_voice)
     cmd = raw_voice
 
     for x in config.OA_ALIAS:
+        # print("x alias: ", x)
         cmd = cmd.replace(x, "").strip()
+        # print("cmd alias: ", cmd)
 
     for x in config.OA_TBR:
+        # print("x tbr: ", x)
         cmd = cmd.replace(x, "").strip()
+        # print("cmd tbr: ", cmd)
 
     return cmd
 
 
 def recognize_cmd(cmd: str):
     rc = {'cmd': '', 'percent': 0}
+    # print("rc: ", rc)
     for c, v in config.OA_CMD_LIST.items():
+        # print(f"c: {c}, v: {v}")
 
         for x in v:
-            vrt = fuzz.ratio(cmd, x)
+            vrt = fuzz.token_set_ratio(cmd, x)
+            # print("vrt: ", vrt)
             if vrt > rc['percent']:
+                # print("rc percent: ", rc["percent"])
                 rc['cmd'] = c
                 rc['percent'] = vrt
 
@@ -49,30 +65,27 @@ def recognize_cmd(cmd: str):
 
 
 def execute_cmd(cmd: str):
-    if cmd == 'yordam':
-        # help
+    if cmd == 'info':
+        # info
         text = "Meni turli xil funksiyalarim bor: .."
         text += "vaqtni aniq aytib berish. .."
         text += "turli programmalarni ochish va yopish. .."
         text += "kompyuterni boshqarish. .."
         tts.oa_speak(text)
-        # pass
     elif cmd == 'ctime':
         # current time
-        now = datetime.datetime.now()
-        text = "Xozir vaqt " + str(now.hour) + " " + str(now.minute)
+        hour = eng2uzb(num2words(datetime.datetime.now().strftime("%H")))
+        minute = eng2uzb(num2words(datetime.datetime.now().strftime("%M")))
+        text = "Xozir vaqt " + str(hour) + "-u, " + str(minute) + ". .."
         tts.oa_speak(text)
 
-    elif cmd == 'joke':
-        jokes = ['Как смеются программисты? ... ехе ехе ехе',
-                 'ЭсКьюЭль запрос заходит в бар, подходит к двум столам и спрашивает .. «м+ожно присоединиться?»',
-                 'Программист это машина для преобразования кофе в код']
-
-        tts.oa_speak(random.choice(jokes))
+    elif cmd == 'terminal':
+        tts.oa_speak("terminalni ochyapman... ..")
+        exec(open("functions/terminal.py").read())
 
     elif cmd == 'open_browser':
-        chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
-        webbrowser.get(chrome_path).open("http://python.org")
+        tts.oa_speak("brazerni ochyapman... ..")
+        exec(open("functions/chrome.py").read())
 
 
 # начать прослушивание команд
